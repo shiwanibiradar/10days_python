0fdbad8466cc85393f2ea63ac25bea6ac20d718c
shiwanib <shiwanib@vm1.example.com>
2020-07-24 16:21:16 +0530

    code

---
+++ day1/if_else/even_odd.py
@@ -0,0 +1,7 @@
+a=int(input("enter the first number"))
+if(a % 2==0 & a > 0):
+	print("this is even number")
+elif(a <= 0):
+	print("neither even or odd")
+else:
+	print("This is odd number")
---
+++ day1/if_else/greatest_number.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+if (a > b):
+	print(a,"is greatest number than",b)
+elif (b > a):
+	print(b,"is greater than",a)
+else:
+	print("Both are same number")
